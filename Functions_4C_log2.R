75025739e714b233d39d4e104e82ba66bc9b944c
aurHin <aurelie.hintermann@unige.ch>
2018-01-14 14:24:04 +0100

    Update Functions_4C_log2.R

--- Functions_4C_log2.R
+++ Functions_4C_log2.R
@@ -1,43 +1,6 @@
-### LIBRARIES
-
-library(plyr)
-library(ggplot2)
-library(graphics)
-#library(GenomicRanges)
-
-### FUNCTIONS
-
-# General functions
-
-#ImportBedGraph<-function(x){return(read.table(x, sep="\t", fill = TRUE,header=FALSE, skip=2, quote="\"", stringsAsFactors=F))}
-
 SelectInterval<-function(dataset){
   return(subset(dataset,dataset[,1]==chrom&dataset[,2]>=coordInf&dataset[,3]<=coordSup))
 }
-
-writeBedGraph<-function(header,table,fname){
-  cat(header,file=fname)
-  write.table(table,fname,col.names = F, row.names = F,quote = F,append = T, sep="\t")
-}
-
-SaveCSV<-function(toSave){
-  ScoreSumCSV<-askInput("Enter the name to save the dataframe in a csv: ")
-  ScoreSumCSV<-paste(ScoreSumCSV,".csv",sep="")
-  write.csv(toSave,ScoreSumCSV)
-}
-
-StringToVar<-function(string){return(eval(parse(text = string)))}
-
-ReWrite<-function(x){
-  head <- read.table(x, nrows = 2, header = FALSE, sep =';', stringsAsFactors = FALSE)
-  head<-unlist(head)
-  head<-paste(head[1],"\n",head[2],"\n",sep="")
-  
-  tab<-read.table(x, sep=" ", fill = TRUE,header=T, skip=2, quote="\"", stringsAsFactors=F)
-  svName<-paste("tab_",unlist(strsplit(x,"[.]"))[1],sep="")
-  writeBedGraph(head,tab,svName)
-}
-# To average replicates
 
 AreSameIntervals<-function(IntV1,IntV2){
   if(nrow(IntV1)==nrow(IntV2)){
@@ -71,13 +34,3 @@ ChgCoord<-function(coord,Rname,add){
   coord$V3[coord$V4==Rname]<-coord$V7[coord$V4==Rname]+add
   return(coord)
 }
-
-RemoveNAfromBG<-function(x,inputFolder,outputFolder){
-  BG_DF<-read.table(paste(inputFolder,x,sep = ""), sep="\t", fill = TRUE,skip=1,header=F, quote="\"", stringsAsFactors=F)
-  BG_DF_woNA<-na.omit(BG_DF)
-  
-  BG_header<-as.character(read.table(paste(inputFolder,x,sep = ""), sep="\t", fill = TRUE,nrows=1,header=F, quote="\"", stringsAsFactors=F))
-  
-  BG_name<-paste(outputFolder,unlist(strsplit(x,"[.]"))[1],"_woNAs",sep = "")
-  writeBedGraph(BG_header,BG_DF_woNA,BG_name)
-  }
